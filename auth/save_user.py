77fd7835295e56c8903a091a62d8adc50d6c1240
Neravahn <prashantc48774.r@gmail.com>
2025-12-14 20:01:25 +0530

    Made a default pfp for users ( they can change it )

--- auth/save_user.py
+++ auth/save_user.py
@@ -6,16 +6,16 @@ from mathhash import hash_password
 DB_PATH = 'database.db'
 
 
-def save(name, username, email, password):
+def save(name, username, email, password, pfp):
 
     hashed = hash_password(password)
 
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    query = "INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?)"
+    query = "INSERT INTO users (name, username, email, password, profile_pic) VALUES (?, ?, ?, ?)"
 
-    cursor.execute(query, (name, username, email, hashed))
+    cursor.execute(query, (name, username, email, hashed, pfp))
 
     conn.commit()
     cursor.close()
