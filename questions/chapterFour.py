0fddfd8ba792f30abb9812a8b50410875d8df018
Neravahn <prashantc48774.r@gmail.com>
2025-12-21 09:20:01 +0530

    Added new questions in another chapter, the previus chapters there was very repetitive things done in that so i thought to reduce it from now, i will be making fewer functions to generate questions iwth same randomness

---
+++ questions/chapterFour.py
@@ -0,0 +1,47 @@
+import random
+
+def equation_1():
+    const1 = random.randint(2, 9)
+    const2 = random.randint(2, 9)
+
+    x = random.randint(2, 9)
+    y = random.randint(2, 9)
+
+    eq_templates = [
+        f"(x / {const1}) + (y / {const2})",
+        f"({const1}x + {const2}y)",
+        f"({const1}x - y)",
+        f"(x + y) / (x - y)",
+        f"x - {const1}y",
+        f"x + {const1}y",
+        f"({const1} / (x + y)) + ({const2} / (x - y))",
+    ]
+
+    sol = {
+        f"(x / {const1}) + (y / {const2})": (x / const1) + (y / const2),
+        f"({const1}x + {const2}y)": (const1 * x) + (const2 * y),
+        f"({const1}x - y)": (const1 * x) - y,
+        f"(x + y) / (x - y)" :(x + y) / (x - y),
+        f"x - {const1}y" : x - (const1 * y),
+        f"x + {const1}y": x + ( const1 * y),
+        f"({const1} / (x + y)) + ({const2} / (x - y))": (const1 / (x + y)) + (const2 / (x - y))
+    }
+
+    lhs1, lhs2 = random.sample(eq_templates, 2)
+
+    rhs1 = round(sol[lhs1], 2)
+    rhs2 = round(sol[lhs2], 2)
+
+
+    templates = [
+        f"The solution set of equations {lhs1} = {rhs1}, {lhs2} = {rhs2} is",
+        f"The values of x and y stisfying the equation {lhs1} = {rhs1}, {lhs2} = {rhs2} are given by the pair", 
+        f"Solve for x and y: {lhs1} = {rhs1} and {lhs2} = {rhs2}",
+        f"The pair satifying the equations {lhs1} = {rhs1}, {lhs2} = {rhs2} are given by",
+        f"The simultaneous equaitions {lhs1} = {rhs1}, {lhs2} = {rhs2} have solutions given by"
+    ]  
+
+    question = random.choice(templates)
+    return question
+
+print(equation_1())
