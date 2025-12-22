ec9cedd5f0078feca6faff1c486193d63db3bf11
Neravahn <prashantc48774.r@gmail.com>
2025-12-22 09:40:24 +0530

    Added 2nd template for 3 varaiables

--- questions/chapterFour.py
+++ questions/chapterFour.py
@@ -1,4 +1,5 @@
 import random
+from fractions import Fraction
 
 def equation_1():
     const1 = random.randint(2, 9)
@@ -21,13 +22,13 @@ def equation_1():
     ]
 
     sol = {
-        f"(x / {const1}) + (y / {const2})": (x / const1) + (y / const2),
-        f"({const1}x + {const2}y)": (const1 * x) + (const2 * y),
-        f"({const1}x - y)": (const1 * x) - y,
-        f"(x + y) / (x - y)" :(x + y) / (x - y),
-        f"x - {const1}y" : x - (const1 * y),
-        f"x + {const1}y": x + ( const1 * y),
-        f"({const1} / (x + y)) + ({const2} / (x - y))": (const1 / (x + y)) + (const2 / (x - y))
+        f"(x / {const1}) + (y / {const2})": Fraction(x, const1) + Fraction(y, const2),
+        f"({const1}x + {const2}y)": Fraction(const1*x + const2*y),
+        f"({const1}x - y)": Fraction(const1*x - y),
+        f"(x + y) / (x - y)" :Fraction((x + y), (x - y)),
+        f"x - {const1}y": Fraction(x - const1*y),
+        f"x + {const1}y": Fraction(x + const1*y),
+        f"({const1} / (x + y)) + ({const2} / (x - y))": Fraction(const1, (x + y)) + Fraction(const2, (x - y))
     }
 
     lhs1, lhs2 = random.sample(eq_templates, 2)
@@ -47,6 +48,7 @@ def equation_1():
     question = random.choice(templates)
     answer = f"(x={x}, y={y})"
     options = []
+    options.append(answer)
     while len(options) < 4:
         option_x = random.randint(2, 9)
         option_y = random.randint(2, 9)
@@ -63,6 +65,71 @@ def equation_1():
     return question, options, answer
 
 
-q, o, a = equation_1()
 
+
+
+def equation_2():
+    const1 = random.randint(2, 20)
+    const2 = random.randint(2, 20)
+    const3 = random.randint(2, 20)
+
+    while True:
+        x = random.randint(2, 20)
+        y = random.randint(2, 20)
+        z = random.randint(2, 20)
+
+        if x!=y!=z:
+            break
+
+    eq_templates = [
+        "xy / (x + y)",
+        "yz / (y + z)",
+        "xz / (x + z)",
+        f"{const1}x + {const2}y + {const3}z",
+        f"{const1}x + {const2}y - {const3}z",
+        f"{const1}x - {const2}y + {const3}z",
+        f"{const1}(x + y) + 2z",
+        f"{const1}x - {const2}(y + z)",
+        f"x + {const1}(x + y - z)"
+    ]
+
+    sol = {
+        "xy / (x + y)": Fraction((x * y), (x + y)),
+        "yz / (y + z)": Fraction((y * z), (y + z)),
+        "xz / (x + z)": Fraction((x * z),  (x + z)),
+        f"{const1}x + {const2}y + {const3}z": (const1 * x) + (const2 * y) + (const3 * z),
+        f"{const1}x + {const2}y - {const3}z": (const1 * x) + (const2 * y) - (const3 * z),
+        f"{const1}x - {const2}y + {const3}z": (const1 * x) - (const2 * y) + (const3 * z),
+        f"{const1}(x + y) + 2z" :(const1 * ( x + y)) + (2 * z),
+        f"{const1}x - {const2}(y + z)": (const1 * x) - (const2 * (y + z)),
+        f"x + {const1}(x + y - z)": x + (const1 * (x + y - z))
+    }
+
+    
+
+    rand_1, rand_2= random.sample(eq_templates, 2)
+    sol_1 = round(sol[rand_1], 2)
+    sol_2 = round(sol[rand_2], 2)
+    question = f"{rand_1} = {sol_1}, {rand_2} = {sol_2}"
+
+    answer = f"(x = {x}, y = {y}, z = {z})"
+    options = []
+    options.append(answer)
+    while len(options) < 4:
+        option_x = random.randint(2, 20)
+        option_y = random.randint(2, 20)
+        option_z = random.randint(2, 20)
+        option = f"(x = {option_x}, y = {option_y}, z = {option_z})"
+        if option not in options and option != answer:
+            options.append(option)
+        else:
+            continue
+
+    random.shuffle(options)
+    return question, options, answer
+q, o, a = equation_2()
 print(q, o, a)
+
+
+
+
